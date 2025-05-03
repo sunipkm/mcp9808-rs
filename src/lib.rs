3cb5fdfdba50d694ff3a177b827dbaf0c20daa4f
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2025-05-02 21:07:27 -0400

    I2C bus type is now phantom data

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ extern crate bit_field;
 extern crate cast;
 extern crate embedded_hal;
 
+use core::marker::PhantomData;
+
 use crate::address::SlaveAddress;
 use crate::error::Error;
 use crate::reg_conf::Configuration;
@@ -33,35 +35,17 @@ pub mod reg_temp_alert_upper;
 pub mod reg_temp_generic;
 
 /// MCP9808 Driver
-pub struct MCP9808 {
+pub struct MCP9808<I2C> {
     addr: u8,
     res: ResolutionVal,
+    _marker: PhantomData<I2C>,
 }
 
-impl MCP9808 {
+impl<I2C> MCP9808<I2C> {
     /// Change i2c address
     pub fn set_address(&mut self, addr: SlaveAddress) -> u8 {
         self.addr = addr.into();
         self.addr
-    }
-
-    pub fn new<I2C>(
-        addr: SlaveAddress,
-        res: ResolutionVal,
-        i2c: &mut I2C,
-    ) -> Result<Self, Error<I2C::Error>>
-    where
-        I2C: I2c<SevenBitAddress>,
-        I2C::Error: Into<Error<I2C::Error>>,
-    {
-        let mut mcp = MCP9808 {
-            addr: addr.into(),
-            res,
-        };
-        let mut conf = reg_conf::new();
-        conf.set_resolution(res);
-        mcp.write_register(conf, i2c)?;
-        Ok(mcp)
     }
 
     /// Get the current temperature resolution
@@ -69,8 +53,30 @@ impl MCP9808 {
     pub fn resolution(&self) -> ResolutionVal {
         self.res
     }
+}
 
-    fn read_register<T, I2C>(&mut self, mut reg: T, i2c: &mut I2C) -> Result<T, Error<I2C::Error>>
+impl<I2C> MCP9808<I2C>
+where
+    I2C: I2c<SevenBitAddress>,
+    I2C::Error: Into<Error<I2C::Error>>,
+{
+    pub fn new(
+        addr: SlaveAddress,
+        res: ResolutionVal,
+        i2c: &mut I2C,
+    ) -> Result<Self, Error<I2C::Error>> {
+        let mut mcp = MCP9808 {
+            addr: addr.into(),
+            res,
+            _marker: PhantomData,
+        };
+        let mut conf = reg_conf::new();
+        conf.set_resolution(res);
+        mcp.write_register(conf, i2c)?;
+        Ok(mcp)
+    }
+
+    fn read_register<T>(&mut self, mut reg: T, i2c: &mut I2C) -> Result<T, Error<I2C::Error>>
     where
         T: prelude::Read,
         I2C: I2c<SevenBitAddress>,
@@ -80,7 +86,7 @@ impl MCP9808 {
         Ok(reg)
     }
 
-    pub fn write_register<R: prelude::Write, I2C>(
+    pub fn write_register<R: prelude::Write>(
         &mut self,
         reg: R,
         i2c: &mut I2C,
@@ -93,7 +99,7 @@ impl MCP9808 {
         Ok(())
     }
 
-    pub fn read_configuration<I2C>(
+    pub fn read_configuration(
         &mut self,
         i2c: &mut I2C,
     ) -> Result<impl Configuration, Error<I2C::Error>>
@@ -104,7 +110,7 @@ impl MCP9808 {
         self.read_register(reg_conf::new(), i2c)
     }
 
-    pub fn read_device_id<I2C>(&mut self, i2c: &mut I2C) -> Result<impl DeviceId, Error<I2C::Error>>
+    pub fn read_device_id(&mut self, i2c: &mut I2C) -> Result<impl DeviceId, Error<I2C::Error>>
     where
         I2C: I2c<SevenBitAddress>,
         I2C::Error: Into<Error<I2C::Error>>,
@@ -112,7 +118,7 @@ impl MCP9808 {
         self.read_register(reg_device_id::new(), i2c)
     }
 
-    pub fn read_manufacturer_id<I2C>(
+    pub fn read_manufacturer_id(
         &mut self,
         i2c: &mut I2C,
     ) -> Result<impl ManufacturerId, Error<I2C::Error>>
@@ -123,10 +129,7 @@ impl MCP9808 {
         self.read_register(reg_manuf_id::new(), i2c)
     }
 
-    pub fn read_resolution<I2C>(
-        &mut self,
-        i2c: &mut I2C,
-    ) -> Result<impl Resolution, Error<I2C::Error>>
+    pub fn read_resolution(&mut self, i2c: &mut I2C) -> Result<impl Resolution, Error<I2C::Error>>
     where
         I2C: I2c<SevenBitAddress>,
         I2C::Error: Into<Error<I2C::Error>>,
@@ -135,10 +138,7 @@ impl MCP9808 {
     }
 
     /// Read temperature register. Its double-buffered so no wait required.
-    pub fn read_temperature<I2C>(
-        &mut self,
-        i2c: &mut I2C,
-    ) -> Result<impl Temperature, Error<I2C::Error>>
+    pub fn read_temperature(&mut self, i2c: &mut I2C) -> Result<impl Temperature, Error<I2C::Error>>
     where
         I2C: I2c<SevenBitAddress>,
         I2C::Error: Into<Error<I2C::Error>>,
@@ -146,7 +146,7 @@ impl MCP9808 {
         self.read_register(reg_temp::new(), i2c)
     }
 
-    pub fn read_alert_critical<I2C>(
+    pub fn read_alert_critical(
         &mut self,
         i2c: &mut I2C,
     ) -> Result<impl CriticalTemperatureAlert, Error<I2C::Error>>
@@ -157,7 +157,7 @@ impl MCP9808 {
         self.read_register(reg_temp_alert_crit::new(), i2c)
     }
 
-    pub fn read_alert_lower<I2C>(
+    pub fn read_alert_lower(
         &mut self,
         i2c: &mut I2C,
     ) -> Result<impl LowerTemperatureAlert, Error<I2C::Error>>
@@ -168,7 +168,7 @@ impl MCP9808 {
         self.read_register(reg_temp_alert_lower::new(), i2c)
     }
 
-    pub fn read_alert_upper<I2C>(
+    pub fn read_alert_upper(
         &mut self,
         i2c: &mut I2C,
     ) -> Result<impl UpperTemperatureAlert, Error<I2C::Error>>
